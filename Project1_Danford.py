a526e404231d056e8c24ea3591f481504d077105
patrick-danford <pdanford@hotmail.com>
2019-12-02 22:00:48 -0800

    Add files via upload

---
+++ Project1_Danford.py
@@ -0,0 +1,5 @@
+inStock = list() # 2D list (row size:10, column size:4)
+alpha = list() # 1D list with 20 elements.
+beta = list() # 1D list with 20 elements.
+gamma = [11, 13, 15, 17]
+delta = [3, 5, 2, 6, 10, 9, 7, 11, 1, 8]
